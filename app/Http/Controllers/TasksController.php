2adf0c847f75c2c3cfd76c9ae25bae6054b8ae4f
ryutaro.hatazoe <liar.milky@gmail.com>
2021-05-28 07:48:04 +0000

    kadai-tasklist2

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,8 +46,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+         //バリデーション
+        $request->validate([
+            'status'=> 'required|max:10',
+            'content'=> 'required',
+            ]);
+            
         //メッセージを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -94,8 +101,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        //バリデーション
+        $request->validate([
+            'status'=> 'required|max:10',
+            'content'=> 'required',
+            ]);
+         
         $task = Task::findOrFail($id);
         
+        $task->status  = $request->status;
         $task->content = $request->content;
         $task->save();
         
