ef48144c9f808e48c13630d028c5c4cc9fe7b73e
ryutaro.hatazoe <liar.milky@gmail.com>
2021-05-28 06:55:46 +0000

    kadai-tasklist

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 
-<h1>メッセージ新規作成ページ</h1>
+<h1>タスク新規作成ページ</h1>
 
 <div class="row">
         <div class="col-6">
             {!!Form::model($task, ['route' => 'tasks.store']) !!}
-                {!! Form::label('content', 'メッセージ:') !!}
+                {!! Form::label('content', 'タスク:') !!}
                 {!! Form::text('content', null , ['class' => 'form-control']) !!}
                 
                 {!! Form::submit('投稿') !!}
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>id = {{ $task->id}} のメッセージ詳細ページ</h1>
+    <h1>id = {{ $task->id}} のタスク詳細ページ</h1>
     
     <table class="table table-borderd">
         <tr>
@@ -10,13 +10,13 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>メッセージ</th>
+            <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
         
     </table>
         {{-- メッセージ編集ページへのリンク --}}
-    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
     
     
     {{-- メッセージ削除フォーム --}}
